22cf28497b88fbe25fa6823e7598c84957d3fbed
Jatin Goel <jatin.iiita@gmail.com>
2019-03-30 07:28:02 +0530

    Initial Django Server Setup

---
+++ server/server/urls.py
@@ -0,0 +1,30 @@
+"""server URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path,include
+from django.views import generic
+from rest_framework_swagger.views import get_swagger_view
+from django.conf.urls.static import static
+from django.conf import settings
+schema_view = get_swagger_view(title='BMS API DOCS')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^api/docs/$', schema_view),
+    url(r'^', include('utility.urls')),
+    url(r'^', include('blogs.urls')),
+]
