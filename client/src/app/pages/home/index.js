31d9dd309679a2cc035b27d35100e476f0cb652c
Strategist-Roy <strategist.roy@gmail.com>
2019-05-11 01:04:32 +0530

    Made Changes

--- client/src/app/pages/home/index.js
+++ client/src/app/pages/home/index.js
@@ -105,7 +105,8 @@ class Home extends React.Component {
                         </Hidden>
                     </div>
                     <Typography className={classes.bannerText}>
-                        We've built a simple yet powerful blog management system which allows you to manage your blogs written on different blogging sites. <br /> The system provides features such as maintaining a user-base, and tracking user's progress out-of-the-box. <br /> The system is completely open-source. We plan to incorporate an 'Opportunities' feature - that provides your <br /> blog's users with job postings bases on the reading habits. 
+                        We've built a simple yet powerful blog management system which allows you to track user's progress out-of-the-box. <br /> 
+                        The system is completely open-source.
                     </Typography>
                 </div>
     
